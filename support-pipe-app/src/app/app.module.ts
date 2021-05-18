9ef4a9eab3ce77a10f1fea1901201c5876ce87be
Simon Lammes <simon.lammes@cofinpro.de>
2021-05-18 16:14:03 +0200

    chore: introduce ngx-translate

--- support-pipe-app/src/app/app.module.ts
+++ support-pipe-app/src/app/app.module.ts
@@ -7,6 +7,9 @@ import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {ReactiveFormsModule} from '@angular/forms';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +18,16 @@ import {ReactiveFormsModule} from '@angular/forms';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        deps: [HttpClient],
+        useFactory: (http: HttpClient) => new TranslateHttpLoader(http, './assets/i18n/', '.json')
+      }
+    })
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- support-pipe-app/src/app/save-issue/save-issue.module.ts
+++ support-pipe-app/src/app/save-issue/save-issue.module.ts
@@ -7,6 +7,7 @@ import {IonicModule} from '@ionic/angular';
 import {SaveIssuePageRoutingModule} from './save-issue-routing.module';
 
 import {SaveIssuePage} from './save-issue.page';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import {SaveIssuePage} from './save-issue.page';
     FormsModule,
     IonicModule,
     SaveIssuePageRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TranslateModule
   ],
   declarations: [SaveIssuePage]
 })
