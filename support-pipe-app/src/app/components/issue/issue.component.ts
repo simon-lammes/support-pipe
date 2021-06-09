fe8792452c6544320ed6d6be337ee74a1e719c05
Simon Lammes <ghostsgandi@gmail.com>
2021-06-09 08:11:06 +0200

    feat: disable newly popped up issue for a short time
    so that the user cannot click it by accident

--- support-pipe-app/src/app/components/issue/issue.component.ts
+++ support-pipe-app/src/app/components/issue/issue.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
 import {Issue} from '../../cross-cutting/issue/issue.model';
 
 @Component({
@@ -6,8 +6,14 @@ import {Issue} from '../../cross-cutting/issue/issue.model';
   templateUrl: './issue.component.html',
   styleUrls: ['./issue.component.css']
 })
-export class IssueComponent {
+export class IssueComponent implements OnChanges {
   @Input() issue: Issue;
+
+  /**
+   * The number of milliseconds a new issue is disabled to prevent the user from
+   * accidentally clicking a newly popped up issue.
+   */
+  @Input() disabledTimeoutForNewIssue = 1000;
 
   /**
    * Whether this issue is a proposal for the user to help fixing.
@@ -15,6 +21,20 @@ export class IssueComponent {
   @Input() isProposal = false;
   @Output() issueRejected = new EventEmitter<boolean>();
   @Output() issueSupported = new EventEmitter<boolean>();
+  isIssueEnabled = true;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const newIssue = changes.issue?.currentValue as Issue;
+    if (newIssue && this.disabledTimeoutForNewIssue) {
+      this.isIssueEnabled = false;
+      setTimeout(() => {
+        const hasIssueNotChangedInMeantime = newIssue.id === this.issue.id;
+        if (hasIssueNotChangedInMeantime) {
+          this.isIssueEnabled = true;
+        }
+      }, this.disabledTimeoutForNewIssue);
+    }
+  }
 
   rejectIssue() {
     this.issueRejected.emit(true);
